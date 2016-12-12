fb61104968efa49343d3f8545d986256bfbf28d6
addx <adpinto95@gmail.com>
2016-12-12 19:50:56 +0000

    folder for the .fis files. will upload later

--- PartA/main.m
+++ PartA/main.m
@@ -5,20 +5,31 @@
 
 function main
     
-    %% -- Controller type
+    % Controller type
     fuzzy_inference_type = menu('Fuzzy Inference Type','Mamdani','Sugeno')
     
-    %% -- Number of rules
+    % Number of rules
     num_rules = menu('Number of rules','9','25')
     
-    %% -- Controller's Reference functions
+    % Controller's Reference functions
     reference_function = menu('Reference Function','Sinusoidal','Square')
 
-    %% -- Controller's Membership functions (Slides p. 383)
+    % Controller's Membership functions (Slides p. 383)
     membership_function = menu('Membership Function','Gaussian','Triangular')
 
-    %% -- 
-    
+    if (fuzzy_inference_type == 1)
+
+    	% Defuzzification methods for Mamdani:
+    	% - centroid
+    	% - bisector
+    	% - mom (middle of maximum)
+    	% - som (smallest of maximum)
+    	% - lom (largest of maximum)
+    	% - Custom
+    	defuzzification_method = menu('Defuzzification method','Centroid','	')
+    else
+    	
+    end
     
 
 end
