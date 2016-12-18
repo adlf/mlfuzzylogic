fdad6fbd80310fcce5c9a46ce7bc23c0a89bc548
addx <adpinto95@gmail.com>
2016-12-18 05:48:05 +0000

    finished all models

--- PartA/main.m
+++ PartA/main.m
@@ -1,37 +1,44 @@
-%
-%   Machine Learning - Fuzzy Control
-%   Main program
-%
+% load all .fis files into the workspace
+% so we can load the models .slx
+clear;
+%mamdani_9_rules_trimf_centroid = readfis('controllers/mamdani_9_rules_trimf_centroid.fis');;
+%mamdani_9_rules_trimf_medmax = readfis('controllers/mamdani_9_rules_trimf_medmax.fis');;
+%mandami_9_rules_gaussmf_centroid = readfis('controllers/mandami_9_rules_gaussmf_centroid.fis');;
+%mandami_9_rules_gaussmf_mom = readfis('controllers/mandami_9_rules_gaussmf_mom.fis');;
+%sugeno_9_rules_gaussmf_wtaver = readfis('controllers/sugeno_9_rules_gaussmf_wtaver.fis');;
+%sugeno_9_rules_gaussmf_wtsum = readfis('controllers/sugeno_9_rules_gaussmf_wtsum.fis');;
+%sugeno_9_rules_trimf_wtaver = readfis('controllers/sugeno_9_rules_trimf_wtaver.fis');;
+%sugeno_9_rules_trimf_wtsum = readfis('controllers/sugeno_9_rules_trimf_wtsum.fis');;
 
-function main
-    
-    % Controller type
-    fuzzy_inference_type = menu('Fuzzy Inference Type','Mamdani','Sugeno')
-    
-    % Number of rules
-    num_rules = menu('Number of rules','9','25')
-    
-    % Controller's Reference functions
-    reference_function = menu('Reference Function','Sinusoidal','Square')
-
-    % Controller's Membership functions (Slides p. 383)
-    membership_function = menu('Membership Function','Gaussian','Triangular')
-
-    if (fuzzy_inference_type == 1)
-
-    	% Defuzzification methods for Mamdani:
-    	% - centroid
-    	% - bisector
-    	% - mom (middle of maximum)
-    	% - som (smallest of maximum)
-    	% - lom (largest of maximum)
-    	% - Custom
-    	defuzzification_method = menu('Defuzzification method','Centroid','	')
-    else
-    	
-    end
-    
-
-end
+%mamdani_25_rules_gaussmf_centroid = readfis('controllers/mamdani_25_rules_gaussmf_centroid.fis');;
+%mamdani_25_rules_gaussmf_medmax = readfis('controllers/mamdani_25_rules_gaussmf_medmax.fis');;
+%mamdani_25_rules_trimf_centroid = readfis('controllers/mamdani_25_rules_trimf_centroid.fis');;
+%mamdani_25_rules_trimf_medmax = readfis('controllers/mamdani_25_rules_trimf_medmax.fis');;
+%sugeno_25_rules_gaussmf_wtaver = readfis('controllers/sugeno_25_gaussmf_wtaver.fis');;
+%sugeno_25_rules_gaussmf_wtsum = readfis('controllers/sugeno_25_gaussmf_wtsum.fis');;
+%sugeno_25_rules_trimf_wtaver = readfis('controllers/sugeno_25_trimf_wtaver.fis');;
+%sugeno_25_rules_trimf_wtsum = readfis('controllers/sugeno_25_trimf_wtsum.fis');;
 
 
+mamdani_9_rules_trimf_medmax = readfis('controllers/mamdani_9_rules_trimf_medmax.fis');
+mamdani_9_rules_gaussmf_medmax = readfis('controllers/mamdani_9_rules_gaussmf_medmax.fis');
+mamdani_9_rules_trimf_centroid = readfis('controllers/mamdani_9_rules_trimf_centroid.fis');
+mamdani_9_rules_gaussmf_centroid = readfis('controllers/mamdani_9_rules_gaussmf_centroid.fis');
+
+sugeno_9_rules_trimf_wtaver = readfis('controllers/sugeno_9_rules_trimf_wtaver.fis');
+sugeno_9_rules_trimf_wtsum = readfis('controllers/sugeno_9_rules_trimf_wtsum.fis');
+sugeno_9_rules_gaussmf_wtsum = readfis('controllers/sugeno_9_rules_gaussmf_wtsum.fis');
+sugeno_9_rules_gaussmf_wtaver = readfis('controllers/sugeno_9_rules_gaussmf_wtaver.fis');
+
+mamdani_25_rules_trimf_medmax = readfis('controllers/mamdani_25_rules_trimf_medmax.fis');
+mamdani_25_rules_gaussmf_medmax = readfis('controllers/mamdani_25_rules_gaussmf_medmax.fis');
+mamdani_25_rules_trimf_centroid =  readfis('controllers/mamdani_25_rules_trimf_centroid.fis');
+mamdani_25_rules_gaussmf_centroid = readfis('controllers/mamdani_25_rules_gaussmf_centroid.fis');
+
+sugeno_25_rules_gaussmf_wtaver = readfis('controllers/sugeno_25_rules_gaussmf_wtaver.fis');
+sugeno_25_rules_gaussmf_wtsum = readfis('controllers/sugeno_25_rules_gaussmf_wtsum.fis');
+sugeno_25_rules_trimf_wtaver = readfis('controllers/sugeno_25_rules_trimf_wtaver.fis');
+sugeno_25_rules_trimf_wtsum = readfis('controllers/sugeno_25_rules_trimf_wtsum.fis');
+
+disp('All .fis files loaded.');
+disp('You can now run the simulink models in the models folder.')
